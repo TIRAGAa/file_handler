b5496213fcef4aa08a742fab4f2eed312f7626c8
Арагац Тирацян <xaker.tank@list.ru>
2026-02-21 03:01:50 +0300

    v0.2 add script

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import csv
+from tabulate import tabulate
 
 parser = argparse.ArgumentParser()
 
@@ -12,9 +14,39 @@ args = parser.parse_args()
 
 
 if args.report == 'average-gdp':
-    for file_name in args.file_names:
-        with open(file_name, 'r') as f:
-            data = f.read()
-        print(data)
+    gdp_data = {}
+    try:
+        gdp_average = {}
+        for file_name in args.file_names:
+            with open(file_name, 'r') as f:
+                data = csv.DictReader(f)
+                for row in data:
+                    country = row['country']
+                    gdp = float(row['gdp'])
+                    if country not in gdp_average:
+                        gdp_average[country] = []
+                    gdp_average[country].append(gdp)
 
-print(args)
+        for country, gdp_list in gdp_average.items():
+            average_gdp = sum(gdp_list) / len(gdp_list)
+            gdp_data[country] = average_gdp
+        formatted_data = []
+
+        for i, (country, gdp) in enumerate(gdp_data.items(), start=1):
+            formatted_gdp = f"{gdp:.2f}"
+            formatted_data.append((i, country, formatted_gdp))
+
+        results = tabulate(
+            formatted_data,
+            headers=['', 'country', 'gdp'],
+            tablefmt='pretty',
+            colalign=('right', 'left', 'right'),
+        )
+    except FileNotFoundError as e:
+        print(f"File not found (Файл не найден): {e}")
+    except Exception as e:
+        print(f"Error reading file (Ошибка чтения файла {file_name}): {e}")
+
+
+print('---- Отчет создан успешно ----')
+print(results)
