1b0ff9f258a0ea4dbeaa55d8b42d6ec4f497d665
Арагац Тирацян <xaker.tank@list.ru>
2026-02-22 14:50:14 +0300

    v0.3 update script: Added data processing to multiple columns.

---
+++ core.py
@@ -0,0 +1,67 @@
+import csv
+from tabulate import tabulate
+
+
+def get_data(files):
+    """Reads data from multiple CSV files and combines it into a single list
+    of dictionaries.
+    Чтение данных из нескольких CSV файлов и объединение их в один список
+    словарей"""
+
+    all_data = []
+    for file in files:
+        try:
+            with open(file, 'r') as f:
+                reader = csv.DictReader(f)
+                all_data.extend(list(reader))
+        except FileNotFoundError as e:
+            print(f"File not found (Файл не найден): {e}")
+        except Exception as e:
+            print(f"An error occurred (Произошла ошибка): {e}")
+    return all_data
+
+
+def process_data(data: list, report_type: str) -> dict:
+    """Processes the data to calculate
+    the average valuesfor the specifiedreport type.
+    Обработка данных для расчета
+    средних значений для указанного типа отчета."""
+
+    report_data = {}
+    for row in data:
+        country = row['country']
+        value = float(row[report_type])
+        if country not in report_data:
+            report_data[country] = []
+        report_data[country].append(value)
+    average_report = {
+        country: sum(values) / len(values)
+        for country, values in report_data.items()
+    }
+    return average_report
+
+
+def sort_report_data(report_data: dict) -> list:
+    """Sorts the report data in descending order based on the average values.
+    Сортировка данных отчета в порядке убывания на основе средних значений."""
+
+    sorted_data = sorted(
+        report_data.items(), key=lambda x: float(x[1]), reverse=True
+    )
+    formatted_data = [
+        (i, country, f"{value:.2f}")
+        for i, (country, value) in enumerate(sorted_data, start=1)
+    ]
+    return formatted_data
+
+
+def main(files: list, report: str):
+    """Main function to execute the data processing and reporting.
+    Основная функция для выполнения обработки данных и создания отчета."""
+
+    report = report.lower().replace('average-', '')
+    data = get_data(files)
+    report_data = process_data(data, report)
+    sorted_data = sort_report_data(report_data)
+    print(tabulate(sorted_data, headers=['', 'country', report],
+                   tablefmt='pretty', colalign=('right', 'left', 'right')))
--- main.py
+++ main.py
@@ -1,52 +1,13 @@
 import argparse
-import csv
-from tabulate import tabulate
+from core import main
 
 parser = argparse.ArgumentParser()
 
-# Options, именованные аргументы
 parser.add_argument('--file', type=str, dest='file_names', nargs='+',
                     help='file names',)
-parser.add_argument('--report', choices=['average-gdp', ...],
-                    help='report type')
+parser.add_argument('--report', dest='report', help='report type')
 
 args = parser.parse_args()
 
-
-if args.report == 'average-gdp':
-    gdp_data = {}
-    try:
-        gdp_average = {}
-        for file_name in args.file_names:
-            with open(file_name, 'r') as f:
-                data = csv.DictReader(f)
-                for row in data:
-                    country = row['country']
-                    gdp = float(row['gdp'])
-                    if country not in gdp_average:
-                        gdp_average[country] = []
-                    gdp_average[country].append(gdp)
-
-        for country, gdp_list in gdp_average.items():
-            average_gdp = sum(gdp_list) / len(gdp_list)
-            gdp_data[country] = average_gdp
-        formatted_data = []
-
-        for i, (country, gdp) in enumerate(gdp_data.items(), start=1):
-            formatted_gdp = f"{gdp:.2f}"
-            formatted_data.append((i, country, formatted_gdp))
-
-        results = tabulate(
-            formatted_data,
-            headers=['', 'country', 'gdp'],
-            tablefmt='pretty',
-            colalign=('right', 'left', 'right'),
-        )
-    except FileNotFoundError as e:
-        print(f"File not found (Файл не найден): {e}")
-    except Exception as e:
-        print(f"Error reading file (Ошибка чтения файла {file_name}): {e}")
-
-
-print('---- Отчет создан успешно ----')
-print(results)
+if __name__ == "__main__":
+    main(args.file_names, args.report)
